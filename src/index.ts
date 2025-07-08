3d3f0c9a66a37d26a23d42967c65aa54913be69b
Gokhan Kurt <krtgkn@gmail.com>
2025-07-08 17:01:08 +0300

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+/**
+ * If possible, put your minimal reproduction in this file, but feel free to
+ * create other files elsewhere in the project if necessary.
+ *
+ * Once you have a minimal reproduction, commit it and push it to a public
+ * GitHub repo.
+ */
+
+import { hey1 } from 'test-import';
+import type { ParsedPath } from 'node:path';
+import { hey2 } from 'test';
+
+const parsed: ParsedPath = {} as any;
